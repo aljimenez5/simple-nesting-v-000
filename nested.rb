0b40dd97fb07852005f1442b6342dbdc67ba5e9f
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-09 02:06:25 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -70,7 +70,7 @@ def adding_matz
 #    :languages => ["LISP", "C"]
 # }
 
-programmer_hash[[Hash]]
+programmer_hash[:yukihiro_matsumoto[Hash[:known_for, "Ruby", :languages["LISP", "C"]]]]
 
 	programmer_hash = 
  		{
